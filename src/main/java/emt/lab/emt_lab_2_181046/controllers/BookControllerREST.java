28e977e2d0840bcd65358e0b8117eaac4280e0be
Panchi99 <Babastrina22@>
2022-04-25 23:15:51 +0200

    Origin url change!

--- src/main/java/emt/lab/emt_lab_2_181046/controllers/BookControllerREST.java
+++ src/main/java/emt/lab/emt_lab_2_181046/controllers/BookControllerREST.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://calm-journey-65315.herokuapp.com/")
 @RequestMapping("/api/books")
 public class BookControllerREST {
 
